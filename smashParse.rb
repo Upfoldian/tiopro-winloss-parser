f363033450df094b08c9a3c121b6e7dfee68eeab
unknown <summonerau@gmail.com>
2014-04-17 10:26:44 +1000

    Added sorted output for win/loss data

--- smashParse.rb
+++ smashParse.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
 
-tioFile = Nokogiri::XML(open('brackets.tio'))
+tioFile = Nokogiri::XML(open('Brackets.tio'))
 
 
 #sets up the id to player nickname hash
@@ -26,15 +26,17 @@ tioFile.xpath("//Game").each do |node|
 			player2 = IDtoPlayer[match.xpath("Player2").text]
 			winner = IDtoPlayer[match.xpath("Winner").text]
 			loser = (player1 == winner ? player2 : player1)
-			puts "Winner: " + IDtoPlayer[winner].to_s + " Loser: " + IDtoPlayer[loser].to_s
 
 			winLoss[winner][:wonAgainst].push(loser)
 			winLoss[loser][:lostAgainst].push(winner)
 		end
 	end
-	winLoss.each_key do |player| 
-		puts "Player " + player + " match record"
-		puts "\tWon against: " + winLoss[player][:wonAgainst].to_s
-		puts "\tLost against: " + winLoss[player][:lostAgainst].to_s
+	file = File.open("RoS3wl.txt", "w")
+	asdf = winLoss.sort_by {|key, val| val[:wonAgainst].length}.reverse
+	asdf = Hash[asdf.map {|key, value| [key, value]}]
+	asdf.each_key do |player| 
+		file.puts "Player " + player + " match record"
+		file.puts "\tWon against: " + winLoss[player][:wonAgainst].to_s
+		file.puts "\tLost against: " + winLoss[player][:lostAgainst].to_s
 	end
 end
