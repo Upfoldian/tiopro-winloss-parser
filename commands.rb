0764980e5fb75a5f7c9d1e9262b7a5712cd4896b
unknown <summonerau@gmail.com>
2014-04-18 22:37:31 +1000

    Fixed winLoss data issue, adding the win/loss functions and are tested working. New issues with adding bracket files other than "Brackets.tio"

---
+++ commands.rb
@@ -0,0 +1,66 @@
+class Commands
+	def self.assign(args)
+		#Usage assign player1 player2
+		#Assigns one alias to another for discrepancies across tournaments
+		# => Should store these assignments so they don't need to be run each time
+		puts "assign"
+	end
+	def self.wins(args)
+		#Usage wins player1 player2 ... playerN
+		#Fetches the 'wonAgainst' array for target players
+		if $winLoss.size == 0
+			puts "Yo, there isn't any data yet. Add a bracket goofhead"
+			return
+		end
+		result = []
+		args.each do |player|
+			puts "#{player} has won against #{$winLoss[player][:wonAgainst]}"
+		end
+	end
+	def self.losses(args)
+		#Usage: losses player1 player2 ... playerN
+		#fetches the 'lostAgainst' array for target players
+		if $winLoss.size == 0
+			puts "Yo, there isn't any data yet. Add a bracket goofhead"
+			return
+		end
+		result = []
+		args.each do |player|
+			puts "#{player} has lost against #{$winLoss[player][:wonAgainst]}"
+		end
+	end
+	def self.help(args)
+		puts self.methods(false)[1..-1]
+	end
+	def self.addBracket(args)
+		#Usage addBracket filePath bracketName
+		puts args[0]
+		tioFile = Nokogiri::XML(open(args[0]))
+		tioFile.xpath("//Players/Player").each do |node|
+			$IDtoPlayer[node.xpath("ID").text] = node.xpath("Nickname").text
+			# Couldn't get the nokogiri node funtions working properly so i used the xpath, it sucks, i know
+		end
+		#gets win/loss data and converts the ID associated with the players involved and replaces
+		#it with their nickname/alias i.e. someNumbers => 'AkiTheBakaQueen'
+		tioFile.xpath("//Game").each do |node|
+			if node.xpath("Name").text.downcase == args[1].to_s.downcase
+				node.xpath("Entrants/Entrant/PlayerID").each do |entrant|
+					if !$winLoss.has_key?($IDtoPlayer[entrant.text])
+						puts "Player '#{$IDtoPlayer[entrant.text]}' was added"
+						$winLoss[$IDtoPlayer[entrant.text]] = {:wonAgainst => [], :lostAgainst => [] }
+					end
+				end
+				node.xpath("Bracket/Matches/Match").each do |match|
+					player1 = $IDtoPlayer[match.xpath("Player1").text]
+					player2 = $IDtoPlayer[match.xpath("Player2").text]
+					winner = $IDtoPlayer[match.xpath("Winner").text]
+					loser = (player1 == winner ? player2 : player1)
+
+					$winLoss[winner][:wonAgainst].push(loser)
+					$winLoss[loser][:lostAgainst].push(winner)
+				end
+			end
+		end
+		#puts $winLoss
+	end
+end
--- inputSystem.rb
+++ inputSystem.rb
@@ -1,71 +1,14 @@
 #Written by Thomas Upfold for smash community
 #TODO:
-# => Find ot why winLoss isn't retaining data
 # => do other functions 
-
+# => addBracket not working for files other than "Brackets.tio" I don't even
 
 require 'nokogiri'
 require_relative 'commands.rb'
-
-players = Hash.new{|hash,key| hash[key] = []}
-winLoss = Hash.new({:wonAgainst => [], :lostAgainst => [] })
-IDtoPlayer = {}
-
-def assign(args)
-	#Usage assign player1 player2
-	#Assigns one alias to another for discrepancies across tournaments
-	# => Should store these assignments so they don't need to be run each time
-	puts "assign"
-end
-def wins(args)
-	#Usage wins player1 player2 ... playerN
-	#Fetches the 'wonAgainst' array for target players
-	if winLoss == {}
-		puts "No win/loss data recorded. Try adding a bracket before running this"
-		return
-	end
-	puts "wins"
-end
-def losses(args)
-	#Usage: losses player1 player2 ... playerN
-	#fetches the 'lostAgainst' array for target players
-	if winLoss == {}
-		puts "No win/loss data recorded. Try adding a bracket before running this"
-		return
-	end
-	puts "losses"
-end
-def help(args)
-	puts self.methods(false)[1..-1]
-end
-def addBracket(args)
-	#Usage addBracket filePath bracketName
-	tioFile = Nokogiri::XML(open(args[0]))
-	tioFile.xpath("//Players/Player").each do |node|
-		IDtoPlayer[node.xpath("ID").text] = node.xpath("Nickname").text
-		# Couldn't get the nokogiri node funtions working properly so i used the xpath, it sucks, i know
-	end
-	#gets win/loss data and converts the ID associated with the players involved and replaces
-	#it with their nickname/alias i.e. someNumbers => 'AkiTheBakaQueen'
-	winLoss = Hash.new({:wonAgainst => [], :lostAgainst => [] })
-	tioFile.xpath("//Game").each do |node|
-		if node.xpath("Name").text.downcase == args[1].to_s.downcase
-			node.xpath("Entrants/Entrant/PlayerID").each do |entrant|
-				winLoss[IDtoPlayer[entrant.text]] = {:wonAgainst => [], :lostAgainst => [] }
-			end
-			node.xpath("Bracket/Matches/Match").each do |match|
-				player1 = IDtoPlayer[match.xpath("Player1").text]
-				player2 = IDtoPlayer[match.xpath("Player2").text]
-				winner = IDtoPlayer[match.xpath("Winner").text]
-				loser = (player1 == winner ? player2 : player1)
-
-				winLoss[winner][:wonAgainst].push(loser)
-				winLoss[loser][:lostAgainst].push(winner)
-			end
-		end
-	end
-	puts "added"
-end
+#These are made global vars because I wanted to offload the commands to another file
+$players = Hash.new{|hash,key| hash[key] = []}
+$winLoss = Hash.new({:wonAgainst => [], :lostAgainst => [] })
+$IDtoPlayer = {}
 
 menuLoop = true
 while (menuLoop)
@@ -75,10 +18,9 @@ while (menuLoop)
 		if (commandToken == 'exit' || commandToken == 'quit') 
 			menuLoop = false
 		else
-			self.send(commandToken, args)
+			Commands.send(commandToken, args)
 		end
 	rescue NoMethodError
 		puts "#{commandToken} is not a valid command. Type 'help' for a list of commands."
 	end
 end
-puts winLoss
